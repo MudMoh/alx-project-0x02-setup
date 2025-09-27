83217f1d7d4c7847d9b968e44d14e8c005ae0829
MudMoh <mudesir@gmail.com>
2025-09-27 23:19:56 +0300

    Implement a PostCard Component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,45 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
+import { type PostProps } from "@/interfaces";
 
-interface ApiPost {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+  const posts = await res.json();
+  return { props: { posts } };
 }
 
-const Posts: React.FC = () => {
-  const [posts, setPosts] = useState<ApiPost[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then(res => res.json())
-      .then(data => {
-        setPosts(data);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <Header />
-      <h1>Posts Page</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        posts.map(post => (
-          <PostCard
-            key={post.id}
-            title={post.title}
-            content={post.body}
-            userId={post.userId}
-          />
-        ))
-      )}
-    </div>
-  );
-};
+const Posts: React.FC<PostProps> = ({ posts }) => (
+  <div>
+    <Header />
+    <h1>Posts Page</h1>
+    {posts.map(post => (
+      <PostCard
+        key={post.id}
+        title={post.title}
+        content={post.body}
+        userId={post.userId}
+      />
+    ))}
+  </div>
+);
 
 export default Posts;
