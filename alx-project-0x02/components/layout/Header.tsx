4f1597d7761582670160b80096fe9cff438b8e69
MudMoh <mudesir@gmail.com>
2025-09-27 11:44:49 +0300

    Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link'; 
 
+
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-600 text-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h3 className="font-bold text-2xl">
-            <Link href="/">Daily Contents</Link>
-        </h3>
+       
         <nav>
           <ul className="flex space-x-6">
             <li className="hover:underline">
-              <Link href="/posts">Posts</Link>
+              <Link href="/">Home</Link>
             </li>
             <li className="hover:underline">
-              <Link href="/users">Users</Link>
+              <Link href="/">About</Link>
             </li>
           </ul>
         </nav>
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,8 @@
+const Home: React.FC =  () => {
+  return (
+    <div>
+   <h1 className="text-xl font-extralight">Home Page</h1>
+    </div>
+  )
+}
+export default Home;
